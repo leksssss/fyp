39dbcf23484128a990a00b65e3f26444cc2a588d
lekssss <lekhashanthinir@gmail.com>
2022-05-05 12:22:45 +0530

    added flask_recaptcha package

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import *
+from flask_recaptcha import ReCaptcha
 
 app = Flask(__name__)
+app.config['RECAPTCHA_SITE_KEY'] = '6Lc7ULofAAAAAMloaPomViWK77IsiM-RgzohLD72'
+app.config['RECAPTCHA_SECRET_KEY'] = '6Lc7ULofAAAAAIcqko0ahowaGEWDRmfF0G2OK8l-'
+recaptcha = ReCaptcha(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("captcha.html")
-
-@app.route('/verify', methods=["GET", "POST"])
-def verify():
-    return "success"
-    #return redirect(url_for("index"))
+    message = '' 
+    if request.method == 'GET':
+        return render_template('captcha.html', message=message)
+    if request.method == 'POST':
+        if recaptcha.verify(): 
+            message = 'Captcha verification passed!' 
+        else:
+            message = 'Please fill out the Captcha!'
+        return render_template('captcha.html', message=message)
 
 if __name__ == '__main__':
     app.run()
